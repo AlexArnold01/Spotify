173b85745804ad1361e906063c0a5983ed47cecc
AlexArnold01 <alar1000@stud.ek.dk>
2025-09-24 10:19:54 +0200

    commit

--- src/Spotify/Genre.java
+++ src/Spotify/Genre.java
@@ -1,18 +1,8 @@
 package Spotify;
 
 public enum Genre {
-    ROCK("Rock"),
-    POP("Pop"),
-    RAP("Rap"),
-    JAZZ("Jazz");
-
-    private String genre;
-
-    Genre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
+    ROCK,
+    POP,
+    RAP,
+    JAZZ
 }
