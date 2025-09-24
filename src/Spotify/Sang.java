a4ed38e0e25e6d5bb34c55187deaadbb3bc06768
Zander3-2 <zaen0001@stud.ek.dk>
2025-09-24 10:30:10 +0200

    Konstruktør, Setters og Getters

--- src/Spotify/Sang.java
+++ src/Spotify/Sang.java
@@ -1,4 +1,22 @@
 package Spotify;
 
 public class Sang {
+    private String title;
+    private String artist;
+    private Genre genre;
+
+    //Constructor
+    Sang(String title, String artist ){
+this.title = title;
+this.artist = artist;
+    }
+    //getters
+public String getTitle() {return title;}
+public String getArtist() {return artist;}
+public Genre getGenre() {return genre;}
+
+    //Setters
+    public void setTitle(String title){this.title = title;}
+    public void setArtist(String artist){this.artist = artist;}
+
 }
